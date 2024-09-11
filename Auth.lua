20fbe580e5cf8336c3ed1215c9469246ffb5b5a2
Vector3-rblx <123206939+Vector3-rblx@users.noreply.github.com>
2024-09-11 17:45:43 -0400

    Update Auth.lua

--- Auth.lua
+++ Auth.lua
@@ -2,6 +2,6 @@ local Auth = {
     ["=<=9<6766765<69<6<7>89"] = {"Damc"}
 }
 return function(requested)
-    print("Auth registered -> sent")
+    print("Auth Service -> returned")
     return table.find(Auth,requested)
 end
