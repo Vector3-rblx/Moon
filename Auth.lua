b5b79d7f6e534963a8cd564f009ba9ab8082aaf9
Vector3-rblx <123206939+Vector3-rblx@users.noreply.github.com>
2024-09-11 17:37:07 -0400

    Update Auth.lua

--- Auth.lua
+++ Auth.lua
@@ -1,4 +1,7 @@
-local Auth = {}
+local Auth = {
+    ["677490624_8894781333453982"] = {"Damc"}
+}
 return function(requested)
+    print("Auth registered -> sent")
     return table.find(Auth,requested)
 end
